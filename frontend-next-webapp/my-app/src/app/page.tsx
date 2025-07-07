651d3c99e070960ba9311ea2d2a2f2acb8f21134
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-07-07 09:06:11 -0400

    Convert geojson to json file

--- frontend-next-webapp/my-app/src/app/page.tsx
+++ frontend-next-webapp/my-app/src/app/page.tsx
@@ -2,13 +2,27 @@
 
 import React from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import axios, { AxiosResponse } from "axios";
 
-const geoUrl = "";
+const geoUrl =
+  "https://raw.githubusercontent.com/SumukhP-dev/Healthcare_Policy_Impact_Tracker/refs/heads/main/frontend-next-webapp/my-app/public/california-json-map.geojson";
+
+let content: AxiosResponse<any, any>;
+
+axios
+  .get(geoUrl)
+  .then((response) => {
+    console.log("Axios is working:", response.data);
+    content = response.data;
+  })
+  .catch((error) => {
+    console.error("Error using Axios:", error);
+  });
 
 export default function MapChart() {
   return (
     <ComposableMap>
-      <Geographies geography={geoUrl}>
+      <Geographies geography={content}>
         {({ geographies }) =>
           geographies.map((geo) => (
             <Geography key={geo.rsmKey} geography={geo} />
