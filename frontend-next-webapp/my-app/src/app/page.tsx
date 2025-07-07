619b13dd6256796ec2f6ab60e2f807d60fc79cbc
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-07-07 15:39:07 -0400

    Add county map to webpage

--- frontend-next-webapp/my-app/src/app/page.tsx
+++ frontend-next-webapp/my-app/src/app/page.tsx
@@ -5,7 +5,7 @@ import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import axios, { AxiosResponse } from "axios";
 
 const geoUrl =
-  "https://raw.githubusercontent.com/SumukhP-dev/Healthcare_Policy_Impact_Tracker/refs/heads/main/frontend-next-webapp/my-app/public/california-json-map.json";
+  "https://raw.githubusercontent.com/SumukhP-dev/Healthcare_Policy_Impact_Tracker/refs/heads/main/frontend-next-webapp/my-app/public/california-json-map2.geojson";
 
 let content: AxiosResponse<any, any>;
 
@@ -21,14 +21,28 @@ axios
 
 export default function MapChart() {
   return (
-    <ComposableMap>
-      <Geographies geography={content}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-    </ComposableMap>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <ComposableMap
+        width={2000}
+        height={900}
+        projectionConfig={{
+          scale: 5250,
+          center: [-119.4179, 36.7783], // Centering on California
+        }}
+      >
+        <Geographies geography={content}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill="#EAEAEC"
+                stroke="#D6D6DA"
+              />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
+    </div>
   );
 }
